0ebae66055a4a56841461e9824c35d91e2281d11
Mikhail <mitetenov@yandex.ru>
2026-08-20 15:02:07 +0400

    chore: remove all legacy Java/Maven artifacts and update README

--- bot/src/main/java/com/vpnsupport/bot/TokenStatsDto.java
+++
@@ -1,5 +0,0 @@
-package com.vpnsupport.bot;
-
-public record TokenStatsDto(Long telegramId, long totalTokens, long promptTokens,
-                             long completionTokens, long requestCount) {
-}
